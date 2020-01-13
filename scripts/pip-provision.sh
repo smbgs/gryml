78650c33bed016bfd3f0f21192733933e6ea3c5a
sovnarkom <admin@somebugs.com>
2020-01-13 11:49:19 +0300

    Remak8s / Versioning / Updated to better support semver and make more sense, while also be more compatible with pypi

---
+++ scripts/pip-provision.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+TITLE="Provisioning Gryml to pip"
+PYBASE="/venv/remak8s/bin/"
+
+echo "$TITLE..."
+
+
+$PYBASE/pip install --upgrade setuptools wheel twine
+
+cd /remak8s/gryml/src/ || return
+
+$PYBASE/python setup.py sdist bdist_wheel
+
+$PYBASE/python -m twine upload --repository-url https://pypi.org/legacy/ dist/*
+
+echo "$TITLE done."
